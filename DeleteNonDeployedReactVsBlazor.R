338f88e5f30748275540326da2383a925748d93f
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2024-04-18 12:24:17 +0200

    :tada: Add: Rcodes for analysis data

---
+++ DeleteNonDeployedReactVsBlazor.R
@@ -0,0 +1,109 @@
+library(readxl)
+react_performance_final <-react_Create_NonDep_per_final <- read_excel("BachelorDocumantion /DataSets /react_Delete_NonDep_per_final.xlsx")
+blazor_performance_final <- blazor_Create_NonDep_per_final <- read_excel("BachelorDocumantion /DataSets /blazor_Delete_NonDep_per_final.xlsx")
+
+print(react_performance_final[3:22, 5])
+react_dataEdge<-as.numeric(unlist(react_performance_final[3:22, 5]))
+blazor_dataEdge<-as.numeric(unlist(blazor_performance_final[3:22, 5]))
+data <- data.frame(
+  performanceEdge = c(blazor_dataEdge, react_dataEdge),
+  frameworkEdge = factor(rep(c("Blazor", "React"), each = 20))
+  
+)
+print(data)
+
+
+print(react_performance_final[3:22, 10])
+react_dataChrome<-as.numeric(unlist(react_performance_final[3:22, 10]))
+blazor_dataChrome<-as.numeric(unlist(blazor_performance_final[3:22, 10]))
+dataChrome <- data.frame(
+  performanceChrome = c(blazor_dataChrome, react_dataChrome),
+  frameworkChrome = factor(rep(c("Blazor", "React"), each = 20))
+  
+)
+print(dataChrome)
+
+
+
+print(react_performance_final[3:22, 15])
+react_dataBrave<-as.numeric(unlist(react_performance_final[3:22, 15]))
+blazor_dataBrave<-as.numeric(unlist(blazor_performance_final[3:22, 15]))
+dataBrave <- data.frame(
+  performanceBrave = c(blazor_dataBrave, react_dataBrave),
+  frameworkBrave = factor(rep(c("Blazor", "React"), each = 20))
+  
+)
+print(dataBrave)
+
+
+layout(matrix(c(1, 2, 3), 1, 3, byrow = TRUE))
+
+
+
+
+col_fill <- c(rgb(92/255, 45/255, 145/255, alpha = 0.7),  # Purple with 50% opacity
+              rgb(97/255, 219/255, 251/255, alpha = 0.7))  # Light blue with 50% opacity
+col_fill2 <- c(rgb(92/255, 45/255, 145/255, alpha = 1),  # Purple with 50% opacity
+               rgb(0/255, 91/255, 150/255, alpha = 0.8))  
+
+
+par(col.axis="black", cex.axis=1.1, font.axis=1.9, cex.lab=1.3)
+boxplot(performanceEdge~ frameworkEdge,
+        data=data,
+        
+        main=expression(bold("Edge")),
+        
+        
+        ylab="Speed Index (ms)",
+        col=col_fill, 
+        border= col_fill2, 
+        xlab = "", 
+        boxwex = 0.7,
+        whiskcol = c("black", "black"), 
+        whisklwd = 2, 
+        lwd =1.2
+        
+        
+        
+)
+
+par(col.axis="black", cex.axis=1.1, font.axis=1.9, cex.lab=1.3)
+boxplot(performanceChrome~frameworkChrome,
+        data=dataChrome,
+        
+        main=expression(bold("Chrome")),
+        
+        
+        ylab = "Speed Index (ms)",
+        col=col_fill, 
+        border = col_fill2, 
+        xlab = "", 
+        boxwex = 0.7,
+        whisklwd = 2 , 
+        whiskcol = c("black", "black"),
+        staplecol =  c("black", "black"),
+        lwd =1.2
+        
+        
+        
+)
+
+
+
+par(col.axis="black", cex.axis=1.1, font.axis=1.9, cex.lab=1.3)
+
+boxplot(performanceBrave~frameworkBrave,
+        data=dataBrave,
+        main=expression(bold("Brave")),
+        
+        ylab="Speed Index (ms)",
+        col= col_fill, 
+        border= col_fill2,
+        xlab = "", 
+        boxwex = 0.7,
+        whiskcol = c("black", "black"),
+        staplecol =  c("black", "black"),
+        whisklwd = 2,
+        lwd =1.2
+)
+
